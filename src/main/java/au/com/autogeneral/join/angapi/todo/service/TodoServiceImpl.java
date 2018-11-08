3d50ca1cf737224f39e17bda10e36d96a6f18757
Rahul Nair <rahul.nair@aginicx.com>
2018-11-09 06:49:19 +1000

    added dummy code to test apis

--- src/main/java/au/com/autogeneral/join/angapi/todo/service/TodoServiceImpl.java
+++ src/main/java/au/com/autogeneral/join/angapi/todo/service/TodoServiceImpl.java
@@ -1,6 +1,7 @@
 package au.com.autogeneral.join.angapi.todo.service;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import org.springframework.stereotype.Service;
 
@@ -14,12 +15,12 @@ public class TodoServiceImpl extends TodoService {
 
     @Override
     public TodoItem create(TodoItemAddRequest body) throws NotFoundException {
-        return null;
+        return new TodoItem(BigDecimal.ONE, "hello", false, new Date().toString());
     }
 
     @Override
     public TodoItem get(BigDecimal id) throws NotFoundException {
-        return null;
+        return new TodoItem(BigDecimal.ONE, "hello", false, new Date().toString());
     }
 
     @Override
